6edffb4a9f6623d05c9b18c99e30e985db7281bb
joaomarcus13 <joaomarcus13@hotmail.com>
2022-05-24 20:51:20 -0300

    init routes

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,47 @@
-export function handler(request, responde) {
-  return responde.end('hello world');
+import config from './config.js';
+import { Controller } from './controller.js';
+import { logger } from './util.js';
+
+const {
+  location,
+  pages: { homeHTML, controllerHTML },
+} = config;
+const controller = new Controller();
+
+async function routes(request, response) {
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/') {
+    response.writeHead(302, { Location: location.home });
+    return response.end();
+  }
+
+  if (method === 'GET' && url === '/home') {
+    const { stream } = await controller.getFileStream(homeHTML);
+    return stream.pipe(response);
+  }
+
+  if (method === 'GET' && url === '/controller') {
+    const { stream } = await controller.getFileStream(controllerHTML);
+    return stream.pipe(response);
+  }
+
+  //files
+  if (method === 'GET') {
+    const { stream, type } = await controller.getFileStream(url);
+    return stream.pipe(response);
+  }
+
+  response.writeHead(404);
+  return response.end();
+}
+
+function handleError(error) {
+  logger.error(error);
+}
+
+export function handler(request, response) {
+  return routes(request, response).catch((error) => {
+    logger.error(error);
+  });
 }
